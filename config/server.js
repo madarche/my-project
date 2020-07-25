0d1e1d5c9eb3ed3954d6a089dc9902d28dd84d34
Marc-Aurèle DARCHE <madarche@users.noreply.github.com>
2020-07-25 15:07:39 +0200

    Initial commit
    after `npx create-strapi-app my-project --quickstart`

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ab2780a8c4a25778bb4ac0cd6957e8fc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6d5ca491-093f-4e3a-bf78-2a907abe4725'
+};
