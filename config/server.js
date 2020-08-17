6dbc586519fa55c18f31e532a5e0afa60a089f11
Marc-Aurèle DARCHE <madarche@users.noreply.github.com>
2020-08-17 09:52:35 +0200

    Add needed url setting

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  //url: 'http://localhost:1337',
+  url: 'http://localhost:1337',
   admin: {
     autoOpen: false,
     auth: {
